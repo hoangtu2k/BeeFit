940aafb4ae71f41204d1c2fb3d87ab923684e7cf
phunghoangtu <thocodeonline@gmail.com>
2024-05-10 21:43:33 +0700

    create project be

---
+++ src/main/java/com/spring/beefit/BeeFitApplication.java
@@ -0,0 +1,13 @@
+package com.spring.beefit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeeFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeeFitApplication.class, args);
+	}
+
+}
