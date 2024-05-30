07e115c98535bc388c28da3cfb4d1407b46b80dd
phunghoangtu <thocodeonline@gmail.com>
2024-05-30 17:55:27 +0700

    find attr

--- src/main/java/com/spring/beefit/repository/DesignRepository.java
+++ src/main/java/com/spring/beefit/repository/DesignRepository.java
@@ -3,6 +3,7 @@ package com.spring.beefit.repository;
 import com.spring.beefit.entity.Design;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,5 +13,8 @@ public interface DesignRepository extends JpaRepository<Design, Integer> {
 
     @Query(value = "Select e from Design e where e.status = 0 order by e.createDate desc")
     List<Design> getAll();
+
+    @Query("Select p from Design p where p.id = :id")
+    Design getById(@Param("id") Integer id);
 
 }
--- src/main/java/com/spring/beefit/repository/HandTypeRepository.java
+++ src/main/java/com/spring/beefit/repository/HandTypeRepository.java
@@ -3,6 +3,7 @@ package com.spring.beefit.repository;
 import com.spring.beefit.entity.HandType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,5 +13,8 @@ public interface HandTypeRepository extends JpaRepository<HandType, Integer> {
 
     @Query(value = "Select e from HandType e where e.status = 0 order by e.createDate desc")
     List<HandType> getAll();
+
+    @Query("Select p from HandType p where p.id = :id")
+    HandType getById(@Param("id") Integer id);
 
 }
